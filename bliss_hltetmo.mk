78698944ba42a18e3f8255493ce74b2f2e50d4f0
Ench0 <ench0@users.noreply.github.com>
2015-08-13 15:49:10 +0100

    blissify

---
+++ bliss_hltetmo.mk
@@ -0,0 +1,10 @@
+$(call inherit-product, device/samsung/hltetmo/full_hlte.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/bliss/config/nfc_enhanced.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
+
+PRODUCT_DEVICE := hlte
+PRODUCT_NAME := bliss_hltetmo
